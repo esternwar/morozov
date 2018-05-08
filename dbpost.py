455eb01057994e0ffb27ff446b22ad82156c8ad9
FIIT4M52 <34235939+FIIT4M52@users.noreply.github.com>
2018-05-08 17:47:32 +0300

    1

---
+++ dbpost.py
@@ -0,0 +1,19 @@
+import sqlite3
+from funny import filling
+
+def append(data,delit):
+    conn = sqlite3.connect('mydb.db', isolation_level=None)
+    cursor = conn.cursor()
+    if(delit == True):
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM person")
+        cursor.execute("VACUUM")
+        cursor.close()
+    else:
+        cursor.executemany('INSERT INTO person VALUES (?,?,?,?,?,?,?)',data)
+        conn.commit()
+
+data = filling()
+append(data,0) #params: data - данные для заполнения(массив), delit - если в значении True, то после записи всё удаляется
+
+print("ff")
