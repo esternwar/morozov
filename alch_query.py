27131c7c02c5afc891962ff8777860b093064fa9
FIIT4M52 <34235939+FIIT4M52@users.noreply.github.com>
2018-05-16 15:26:40 +0300

    1

--- alch_query.py
+++ alch_query.py
@@ -2,17 +2,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from create_db import Person
 
-engine = create_engine('sqlite:///main.db', echo=True)
+def query():
 
-# create a Session
-Session = sessionmaker(bind=engine)
-session = Session()
+    engine = create_engine('sqlite:///main.db', echo=True)
+
+    # create a Session
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
 
-query = session.query(Person.name).order_by(Person.name)
-for name in query:
-    print(name)
+    query = session.query(Person.name).order_by(Person.name)
+    for name in query:
+        print(name)
 
-query = session.query(Person.name).filter(Person.placedOrder != 0).filter(Person.area == "Нижегородский").order_by(Person.name)
-for name in query:
-    print(name)
+    query = session.query(Person.name).filter(Person.placedOrder != 0).filter(Person.area == "Нижегородский").order_by(Person.name)
+    for name in query:
+        print(name)
+
+
+query()
